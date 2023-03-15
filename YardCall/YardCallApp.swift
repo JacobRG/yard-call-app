fd6609fe61cef315fa324c6265e00397010ba7ec
JacobRG <jacobgambrell@gmail.com>
2023-03-15 15:04:33 -0400

    Initial Commit

---
+++ YardCall/YardCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  YardCallApp.swift
+//  YardCall
+//
+//  Created by Jacob Gambrell on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct YardCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
